6fa4e207365ec0e5e13c9eba4d02de4482e1944e
Caio <caiojulian@hotmail.com>
2022-02-26 23:37:26 -0300

    config jest

--- jest.config.js
+++ jest.config.js
@@ -9,10 +9,12 @@ module.exports = {
     '^.+\\.js$': 'babel-jest',
     '.*\\.(vue)$': 'vue-jest',
   },
-  collectCoverage: true,
+  collectCoverage: false,
   collectCoverageFrom: [
     '<rootDir>/components/**/*.vue',
     '<rootDir>/pages/**/*.vue',
+    '<rootDir>/layouts/**/*.vue',
+    '<rootDir>/store/**/*.js',
   ],
   testEnvironment: 'jsdom',
 }
